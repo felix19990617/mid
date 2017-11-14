d4c599dcdc068b94c635a45cd5db6b31f231d2bd
felix19990617 <felix3157538@gmail.com>
2017-11-14 14:13:37 +0800

    Numbers

---
+++ src/mid/fju/com/Numbers.java
@@ -0,0 +1,13 @@
+package mid.fju.com;
+
+public class Numbers 
+{
+	public static void main(String[] args)
+	{
+		int n=1;
+	    for(int n=1;i<10;n++)
+	    	System.out.println();
+	}
+	System.out.println();
+
+}
